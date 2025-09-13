3f3e61eda6e403562e1bf77e70cb76d2ea0847f2
Mayank_Chaudhary <132121447+mayankjaat-45@users.noreply.github.com>
2025-09-13 10:06:22 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "https://taskify-backend-hsac.onrender.com",
   withCredentials: true,
 });
 
